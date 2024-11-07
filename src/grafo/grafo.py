519a2f455a41698c97abda2eb4bff38ff97e38e2
OArthr <130790176+OArthr@users.noreply.github.com>
2024-11-07 20:50:19 -0300

    Update grafo.py

--- src/grafo/grafo.py
+++ src/grafo/grafo.py
@@ -1,37 +1,43 @@
+from random import *
+
 class Vertice:
-    def __init__(self, n, lixo, rato, gato, cachorro):
+    def __init__(self,n):                                   # Construtor do Vértice
         self.n = n
-        self.lixo = lixo
-        self.rato = rato
-        self.gato = gato
-        self.cachorro = cachorro
-
-    def __str__(self):
-        return f"({self.n}, l={self.lixo}, r={self.rato}, g={self.gato}, c={self.cachorro})"
-
-class Grafo:
-    def __init__(self, ponderado=False, direcionado=False):
-        self.vertices = {}
-        self.adjacencias = {}
-        self.ponderado = ponderado
-        self.direcionado = direcionado
-
-    def add_vertice(self, n, lixo, rato, gato, cachorro):
-        if n not in self.vertices:
-            vertice = Vertice(n, lixo, rato, gato, cachorro)
-            self.vertices[n] = vertice
-            self.adjacencias[n] = []
-
-    def add_aresta(self, n1, n2, peso=1):
-        if n1 in self.vertices and n2 in self.vertices:
-            if self.ponderado:
-                self.adjacencias[n1].append((n2,peso))
-                self.adjacencias[n2].append((n1,peso)) if not self.direcionado else None
-            else:
-                self.adjacencias[n1].append((n2))
-                self.adjacencias[n2].append((n1)) if not self.direcionado else None
+        self.lixo = randint(0,10)                           # Adiciona lixo e animais no momento de criação
+        self.rato = 1 if randint(1,2) == 1 else 0           
+        self.gato = 1 if randint(1,4) == 1 else 0
+        self.cachorro = 1 if randint(1,10) == 1 else 0
     
-    def buscaLarg(self,v):
+    def __str__(self):                                      # Põe suas informações em formato de string se chamar print(vertice)
+        return f"{self.n}, l={self.lixo}, r={self.rato}, g={self.gato}, c={self.cachorro}" 
+    
+class Grafo:
+    def __init__(self, direcionado=False):                  # Construtor do Grafo
+        self.vertices = {}      # Dicionario de vértices, basicamente usado como um vetor normal, porém com mais utilidades
+        self.adjacencias = {}   # Dicionario de adjacencias, cada chave representa o vertice, e os itens por chave são seus vizinhos
+        self.pesos = []         # Declarador da matriz de pesos
+        
+        self.direcionado = direcionado
+    
+    def add_vertices(self, qtd):            # Cria e adiciona todos os vértices automaticamente
+        for i in range(qtd):
+            self.vertices[i] = Vertice(i)
+            self.adjacencias[i] = []
+        
+        self.pesos = [[0 for _ in range(qtd)] for _ in range(qtd)]  # Gera a matriz de pesos para entao colocar valores iniciais
+        for i in range(qtd):                                        # (É melhor assim mesmo)
+            for j in range(qtd):
+                self.pesos[i][j] = float('inf') if i!=j else 0
+    
+    def add_aresta(self, v1, v2, peso=1):               # Adiciona as arestas no dicionario de adjacencia
+        if v1 in self.vertices and v2 in self.vertices: # Se os vertices existirem no grafo:
+            self.adjacencias[v1].append(v2)             # Coloca v2 como item da chave v1, v2 vizinho de v1
+            self.pesos[v1][v2] = peso
+            if not self.direcionado:                    # Se não for direcionado, faz a operação simétrica
+                self.adjacencias[v2].append(v1)
+                self.pesos[v2][v1] = peso
+        
+    def buscaLarg(self,v):              # Busca em largura normal
         visitados = []
         fila = [v]
         ordem_visita = []
@@ -47,31 +53,30 @@ class Grafo:
                         fila.append(v)
 
         return ordem_visita
+    
+    def printpeso(self):        # Só printa os pesos
+        n = len(self.vertices)
+        for i in range(n):
+            print(f"{self.pesos[i]}")
 
-    """ def print(self):
-        for v in self.adjacencias:
-            print(f"[{self.vertices[v]}] -> ",end='')
-            print(*self.adjacencias[v],sep=" -> ") """
-
-
-    def __str__(self):
+    
+    def __str__(self):              # Transforma o grafo em string
         resultado = "Grafo:\n"
-        for n, v in self.vertices.items():
-            adjacentes = [self.vertices[n_adj] for n_adj,x in self.adjacencias[n]]
-            resultado += f"{v} -> {', '.join(str(adj) for adj in adjacentes)}\n"
+        for v in self.vertices:     # Para cada chave no dicionario de vertices:
+            resultado += f"{v}|"    # Coloca essa chave no inicio da linha
+            for w in self.adjacencias[v]:   # Pra cada vizinho dessa chave no dicionario de adjacencia:
+                resultado += f" -{self.pesos[v][w]}-> ({w})"    # Coloca todos eles depois de 'v|', separados por uma seta que mostra o peso da aresta 'vw'
+            resultado += "\n"   # Nova linha
         return resultado
 
+g = Grafo()
+g.add_vertices(4)
 
-# Exemplo de uso
-g = Grafo(True)
-g.add_vertice(1,10,1,1,1)
-g.add_vertice(2,0,1,0,0)
-g.add_vertice(3,5,3,2,1)
-g.add_vertice(4,3,10,2,2)
+g.add_aresta(0,1,3)
+g.add_aresta(1,2,2)
+g.add_aresta(2,3,8)
+g.add_aresta(1,3,4)
 
 
-g.add_aresta(1, 2)
-g.add_aresta(2, 3)
-g.add_aresta(2,4)
-print(g.adjacencias)
+g.printpeso()
 print(g)
